f3b351ef7a693b8116ce7c8fd747ff4b3b7115df
Gus Prevas <kprevas@gmail.com>
2018-01-20 15:04:58 -0500

    mapper 0 CHR RAM

--- src/cartridge/mapper0.rs
+++ src/cartridge/mapper0.rs
@@ -5,6 +5,7 @@ use cartridge::NametableMirroring;
 use cartridge::NametableMirroring::*;
 
 use std::cmp::max;
+use hex_slice::AsHex;
 
 struct Mapper0Cpu {
     prg_rom: Vec<u8>,
@@ -14,17 +15,20 @@ struct Mapper0Cpu {
 struct Mapper0Ppu {
     chr_rom: Vec<u8>,
     mirroring: NametableMirroring,
+    uses_chr_ram: bool,
 }
 
 pub fn read(header: &Header, prg_rom: &[u8], chr_rom: &[u8]) -> Cartridge {
+    let uses_chr_ram = chr_rom.len() == 0;
     Cartridge {
         cpu_bus: Box::new(Mapper0Cpu {
             prg_rom: prg_rom.to_vec(),
             prg_ram: vec![0; (u16::from(max(header.prg_ram_blocks, 1)) * 0x2000) as usize],
         }),
         ppu_bus: Box::new(Mapper0Ppu {
-            chr_rom: chr_rom.to_vec(),
+            chr_rom: if uses_chr_ram { vec!(0; 0x2000) } else { chr_rom.to_vec() },
             mirroring: if header.flags_6 & 0x1 == 1 { Vertical } else { Horizontal },
+            uses_chr_ram,
         }),
     }
 }
@@ -64,8 +68,15 @@ impl CartridgeBus for Mapper0Ppu {
         }
     }
 
-    fn write_memory(&mut self, address: u16, _value: u8) {
-        panic!("bad memory write 0x{:04X}", address);
+    fn write_memory(&mut self, address: u16, value: u8) {
+        if self.uses_chr_ram {
+            match address {
+                0x0000 ... 0x1FFF => self.chr_rom[address as usize] = value,
+                _ => panic!("bad memory write 0x{:04X}", address),
+            }
+        } else {
+            panic!("bad memory write 0x{:04X}", address);
+        }
     }
 
     fn mirror_nametable(&self, address: u16) -> u16 {
