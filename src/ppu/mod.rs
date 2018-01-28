e47779cbd1e462ae7b98c7e764221eed393ba4cf
Gus Prevas <kprevas@gmail.com>
2018-01-27 21:54:22 -0500

    add TODO

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -232,6 +232,7 @@ impl<'a> Ppu<'a> {
     }
 
     fn draw_pixel(&mut self) {
+        // TODO: color emphasis
         let mut palette: u16 = 0;
         let mut obj_palette: u16 = 0;
         let mut obj_priority = false;
