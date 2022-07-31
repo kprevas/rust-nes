cdfcd1efa6fc691bdfa424482d6cec24fc89874c
Gus Prevas <kprevas@gmail.com>
2022-07-31 14:18:37 -0400

    vdp control port address writing

--- src/gen/vdp/bus.rs
+++ src/gen/vdp/bus.rs
@@ -1,3 +1,46 @@
+enum AddrMode {
+    Read,
+    Write,
+}
+
+enum AddrTarget {
+    VRAM,
+    CRAM,
+    VSRAM,
+}
+
+struct Addr {
+    mode: AddrMode,
+    target: AddrTarget,
+    addr: u16,
+    vram_to_vram: bool,
+    dma: bool,
+}
+
+impl Addr {
+    fn from_u32(val: u32) -> Addr {
+        let addr = (((val & 0b11) << 14) | ((val >> 16) & 0x3FFF)) as u16;
+        let vram_to_vram = ((val >> 6) & 0b1) == 1;
+        let dma = ((val >> 7) & 0b1) == 1;
+        let (mode, target) = match (((val >> 4) & 0b11) << 2) | (val >> 30) {
+            0b0000 => (AddrMode::Read, AddrTarget::VRAM),
+            0b0001 => (AddrMode::Write, AddrTarget::VRAM),
+            0b1000 => (AddrMode::Read, AddrTarget::CRAM),
+            0b0011 => (AddrMode::Write, AddrTarget::CRAM),
+            0b0100 => (AddrMode::Read, AddrTarget::VSRAM),
+            0b0101 => (AddrMode::Write, AddrTarget::VSRAM),
+            _ => panic!(),
+        };
+        Addr {
+            mode,
+            target,
+            addr,
+            vram_to_vram,
+            dma,
+        }
+    }
+}
+
 struct Status {
     fifo_empty: bool,
     fifo_full: bool,
@@ -31,7 +74,9 @@ pub struct VdpBus {
     status: Status,
     beam_vpos: u16,
     beam_hpos: u16,
-    interlace_mode: bool, // TODO put in mode register
+    interlace_mode: bool,
+    // TODO put in mode register
+    addr: Addr,
 }
 
 impl VdpBus {
@@ -52,15 +97,24 @@ impl VdpBus {
             beam_vpos: 0,
             beam_hpos: 0,
             interlace_mode: false,
+            addr: Addr {
+                mode: AddrMode::Read,
+                target: AddrTarget::VRAM,
+                addr: 0,
+                vram_to_vram: false,
+                dma: false,
+            },
         }
     }
 
     pub fn read_byte(&mut self, addr: u32) -> u8 {
         match addr {
-            0xC004 | 0xC006 | 0xC008 | 0xC00A | 0xC00C | 0xC00E =>
-                (self.read_word(addr) >> 8) as u8,
-            0xC005 | 0xC007 | 0xC009 | 0xC00B | 0xC00D | 0xC00F =>
-                (self.read_word(addr - 1) & 0xFF) as u8,
+            0xC004 | 0xC006 | 0xC008 | 0xC00A | 0xC00C | 0xC00E => {
+                (self.read_word(addr) >> 8) as u8
+            }
+            0xC005 | 0xC007 | 0xC009 | 0xC00B | 0xC00D | 0xC00F => {
+                (self.read_word(addr - 1) & 0xFF) as u8
+            }
             _ => panic!(),
         }
     }
@@ -68,12 +122,14 @@ impl VdpBus {
     pub fn read_word(&mut self, addr: u32) -> u16 {
         match addr {
             0xC004 | 0xC006 => self.status.to_u16(),
-            0xC008 | 0xC00A | 0xC00C | 0xC00E => if self.interlace_mode {
-                ((self.beam_vpos >> 1) << 9)
-                    | (self.beam_vpos & 0b100000000)
-                    | ((self.beam_hpos >> 1) & 0xFF)
-            } else {
-                (self.beam_vpos << 8) | ((self.beam_hpos >> 1) & 0xFF)
+            0xC008 | 0xC00A | 0xC00C | 0xC00E => {
+                if self.interlace_mode {
+                    ((self.beam_vpos >> 1) << 9)
+                        | (self.beam_vpos & 0b100000000)
+                        | ((self.beam_hpos >> 1) & 0xFF)
+                } else {
+                    (self.beam_vpos << 8) | ((self.beam_hpos >> 1) & 0xFF)
+                }
             }
             _ => panic!(),
         }
@@ -88,20 +144,32 @@ impl VdpBus {
 
     pub fn write_byte(&mut self, addr: u32, _data: u8) {
         match addr {
+            0xC0004 | 0xC0005 => panic!(), // TODO: allowed?
             _ => panic!(),
         }
     }
 
     pub fn write_word(&mut self, addr: u32, _data: u16) {
         match addr {
-            0xC0011 | 0xC0013 | 0xC0015 | 0xC0017 => {}, // TODO: PSG
-            0xC001C | 0xC001E => {}, // TODO: debug register
+            0xC0004 => {
+                // TODO: set register
+            }
+            0xC0011 | 0xC0013 | 0xC0015 | 0xC0017 => {} // TODO: PSG
+            0xC001C | 0xC001E => {}                     // TODO: debug register
             _ => panic!(),
         }
     }
 
-    pub fn write_long(&mut self, addr: u32, _data: u32) {
+    pub fn write_long(&mut self, addr: u32, data: u32) {
         match addr {
+            0xC0004 => {
+                if data >> 14 == 0b10 {
+                    self.write_word(addr, (data >> 16) as u16);
+                    self.write_word(addr, (data & 0xFFFF) as u16);
+                } else {
+                    self.addr = Addr::from_u32(data);
+                }
+            }
             _ => panic!(),
         }
     }
