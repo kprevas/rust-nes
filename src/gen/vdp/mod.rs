21cf804a68bb1bd556a8a296c8783160095f3294
Gus Prevas <kprevas@gmail.com>
2022-07-31 16:13:47 -0400

    vdp struct and memory

--- src/gen/vdp/mod.rs
+++ src/gen/vdp/mod.rs
@@ -1,1 +1,24 @@
+use std::cell::RefCell;
+
+use gen::vdp::bus::VdpBus;
+
 pub mod bus;
+
+struct Vdp<'a> {
+    vram: Box<[u8]>,
+    cram: Box<[u8]>,
+    vsram: Box<[u8]>,
+
+    bus: &'a RefCell<VdpBus>,
+}
+
+impl<'a> Vdp<'a> {
+    pub fn new(bus: &RefCell<VdpBus>) -> Vdp {
+        Vdp {
+            vram: vec![0; 0x10000].into_boxed_slice(),
+            cram: vec![0; 0x80].into_boxed_slice(),
+            vsram: vec![0; 0x50].into_boxed_slice(),
+            bus,
+        }
+    }
+}
