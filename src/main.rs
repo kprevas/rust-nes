97ec0294ef61950a11a93dddce683bd0ea74883c
Gus Prevas <kprevas@gmail.com>
2022-08-06 14:39:24 -0400

    fix CLI config

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,16 @@ fn main() {
 
     let matches = clap_app!(myapp =>
         (@subcommand disassemble =>
-            (about: "disassemble a .nes file")
-            (@arg INPUT: "the input file to use")
+            (about: "disassemble a ROM file")
             (@arg OUTPUT: -o "the output file (stdout if not provided)")
         )
         (@subcommand run =>
-            (about: "run a .nes file")
-            (@arg INPUT: "the input file to use")
+            (about: "load and run a ROM")
             (@arg instrument_cpu: -c "instruments CPU")
             (@arg instrument_ppu: -p "instruments PPU")
             (@arg bench_mode: -b "runs in benchmark mode")
         )
-        (@subcommand gen =>
-            (about: "run a .md file")
-            (@arg INPUT: "the input file to use")
-            (@arg instrument_cpu: -c "instruments CPU")
-            (@arg bench_mode: -b "runs in benchmark mode")
-        )
+        (@arg INPUT: "the input file to use")
     )
         .get_matches();
     emu::run(matches);
