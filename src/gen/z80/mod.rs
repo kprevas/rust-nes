b7df2505b39dfdbd3532bbba651f9384362a3be4
Gus Prevas <kprevas@gmail.com>
2022-09-04 12:21:41 -0400

    z80: RLCA

--- src/gen/z80/mod.rs
+++ src/gen/z80/mod.rs
@@ -698,6 +698,14 @@ impl Cpu<'_> {
                     5
                 };
             }
+            Opcode::RLCA => {
+                let result = self.a[self.af_bank].rotate_left(1);
+                self.a[self.af_bank] = result;
+                self.set_flag(CARRY, result & 0x1 > 0);
+                self.set_flag(SUBTRACT, false);
+                self.set_flag(HALF_CARRY, false);
+                self.cycles_to_next += 4;
+            }
             Opcode::RRCA => {
                 let result = self.a[self.af_bank].rotate_right(1);
                 self.a[self.af_bank] = result;
