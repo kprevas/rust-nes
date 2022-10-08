12d226becf0f001cc4a41c9457fc9786fe4e74f9
Gus Prevas <kprevas@gmail.com>
2022-10-08 15:31:25 -0400

    m68k: remove opcode decoding test

--- tests/m68k_tests.rs
+++ tests/m68k_tests.rs
@@ -14,23 +14,6 @@ use emu::gen::vdp::bus::VdpBus;
 use emu::window::Cpu as cpuw;
 
 #[test]
-fn opcode_decoding() {
-    let test_cases = json::parse(include_str!("m68k/68000ops.json"));
-    for (opcode_str, expected_val) in test_cases.unwrap().entries() {
-        let expected = expected_val.as_str().unwrap();
-        let opcode_hex = u16::from_str_radix(opcode_str, 16).unwrap();
-        let opcode = emu::gen::m68k::opcodes::opcode(opcode_hex);
-        assert_eq!(
-            format!("{}", opcode),
-            expected,
-            "{:04X} {:016b}",
-            opcode_hex,
-            opcode_hex
-        );
-    }
-}
-
-#[test]
 fn abcd_sbcd() {
     run_json_test(json::parse(include_str!("m68k/abcd_sbcd.json")).unwrap());
 }
